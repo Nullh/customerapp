8106fc161a83491ec079b5069f56c0639a51fd90
Neil <neil@openprojects.co.uk>
2017-04-24 11:03:30 +0100

    Fixed backticks

--- setupDB.sql
+++ setupDB.sql
@@ -3,10 +3,10 @@ CREATE DATABASE sessionStore;
 CREATE USER sessionUser IDENTIFIED BY 'oCj4yoE5n';
 GRANT SELECT, UPDATE, DELETE, INSERT, CREATE, ALTER ON sessionStore.* TO sessionUser;
 CREATE USER test IDENTIFIED BY 'oCj4yoE5n';
-CREATE TABLE `testdb`.`users` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(45) NOT NULL,
-  `last_name` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`));
+CREATE TABLE 'testdb'.'users' (
+  'id' INT NOT NULL AUTO_INCREMENT,
+  'first_name' VARCHAR(45) NOT NULL,
+  'last_name' VARCHAR(45) NOT NULL,
+  'email' VARCHAR(45) NOT NULL,
+  PRIMARY KEY ('id'));
 GRANT SELECT, INSERT, UPDATE, DELETE ON testdb.users TO test;
